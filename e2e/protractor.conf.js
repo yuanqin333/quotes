1e889f2eddf197b67d8a4463111afe88c547956d
kangs <kangs>
2020-01-11 13:51:27 -0800

    added puppetteer support

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,6 +2,9 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const { JUnitXmlReporter } = require('jasmine-reporters');
+
+process.env.CHROME_BIN = process.env.CHROME_BIN || require("puppeteer").executablePath();
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -9,7 +12,12 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+
+    chromeOptions: {
+      args: ["--headless", "--disable-gpu", "--window-size=1200,900"],
+      binary: process.env.CHROME_BIN
+    }
   },
   //directConnect: true,
   baseUrl: 'http://localhost:4300/',
@@ -24,5 +32,10 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    var junitReporter = new JUnitXmlReporter({
+      savePath: require('path').join(__dirname, './junit'),
+      consolidateAll: true
+    });
+    jasmine.getEnv().addReporter(junitReporter);
   }
 };
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -2,6 +2,8 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  const puppeteer = require('puppeteer');
+  process.env.CHROME_BIN = puppeteer.executablePath();
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -10,22 +12,26 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'text-summary', 'cobertura','json','text'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: '../junit'
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
